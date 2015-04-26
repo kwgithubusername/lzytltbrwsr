86143b4bc02858fd6ebe5e3d3b324145e9c42319
woudini <wukl011@gmail.com>
2015-04-25 21:40:49 -0500

    indent by layoutmargins instead of by changing constraints

--- redditAPI/RAPThreadCommentTableViewCell.m
+++ redditAPI/RAPThreadCommentTableViewCell.m
@@ -28,37 +28,11 @@
     
     if (self.customIndentationLevel != 0)
     {
-//        [self.usernameLabel removeConstraint:self.usernameLabel.constraints[0]];
-//        [self.commentLabel removeConstraint:self.commentLabel.constraints[0]];
-//        
-//        NSLog(@"usernamelabel constriants are %@", self.usernameLabel.constraints);
-//        NSLog(@"commentlabel constriants are %@", self.commentLabel.constraints);
-        
-        self.usernameLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        self.commentLabel.translatesAutoresizingMaskIntoConstraints = NO;
         
         int indention = self.customIndentationLevel*5;
         
-        NSString *indentationStringUsername = [[NSString alloc] initWithFormat:@"H:|-%d-[usernameLabel]", indention];
-        NSString *indentationStringComment = [[NSString alloc] initWithFormat:@"H:|-%d-[commentLabel]", indention];
-        NSLog(@"indentation is %@", indentationStringComment);
-        NSDictionary *viewsDictionary = @{ @"usernameLabel" : self.usernameLabel, @"commentLabel" : self.commentLabel};
-        
-//        [self.contentView addConstraints:[NSLayoutConstraint
-//                                   constraintsWithVisualFormat:@"V:|-4-[usernameLabel]-[commentLabel]-4-|"
-//                                   options:0
-//                                   metrics:nil
-//                                   views:viewsDictionary]];
-        [self.contentView addConstraints:[NSLayoutConstraint
-                                   constraintsWithVisualFormat:indentationStringComment
-                                   options:0
-                                   metrics:nil
-                                   views:viewsDictionary]];
-        [self.contentView addConstraints:[NSLayoutConstraint
-                                   constraintsWithVisualFormat:indentationStringUsername
-                                   options:0
-                                   metrics:nil
-                                   views:viewsDictionary]];
+        self.layoutMargins = UIEdgeInsetsMake(4, indention, 4, 4);
+        self.contentView.layoutMargins = self.layoutMargins;
     }
 }
 
